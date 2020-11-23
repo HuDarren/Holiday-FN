bc7a0157624402f03d169743cc19fad824fd4a53
Darren Hu <darren.hu3@gmail.com>
2020-11-23 10:32:32 -0500

    updated folders and packages

--- client/index.js
+++ client/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-dom";
+import { Router } from "react-router-dom";
+import history from "./history";
+import store from "./store";
+// import reportWebVitals from "./reportWebVitals";
+import "./socket";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <Provider store={store}>
+  <Router history={history}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
+  // </Provider>,
+  document.getElementById("app")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
