c9c842d2d4b37eecf313c0e9d2c3edbd753e56a4
Darren Hu <darren.hu3@gmail.com>
2020-11-24 16:50:17 -0500

    styling added

--- src/components/UserPage/user-profile.js
+++ src/components/UserPage/user-profile.js
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  padding: 50px;
+  margin-bottom: 20px;
+  margin-left: 100px;
+  margin-right: 100px;
+`;
 
 function UserProfile() {
   return (
-    <div>
+    <Container>
       <div>Picture</div>
       <div>Name</div>
       <div>Username</div>
-    </div>
+    </Container>
   );
 }
 
