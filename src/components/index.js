e1f05097b7287b1938d301d9d1367d941b1f9b3e
Darren Hu <darren.hu3@gmail.com>
2020-11-26 19:05:43 -0500

    added userform to export

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as UserHome } from "./UserPage/user-home";
 export { default as LandingHome } from "./LandingPage/landing-home";
 export { Login, Signup } from "./LoginPage/auth-form";
+export { default as UserProfileForm } from "./UserPage/user-profileForm";
