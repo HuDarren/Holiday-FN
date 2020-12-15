63019b8f3f0218229d4d7a337fa56f5fff7d7c88
Darren Hu <darren.hu3@gmail.com>
2020-12-15 13:21:40 -0500

    group form updates

--- src/components/GroupPage/group-formB.js
+++ src/components/GroupPage/group-formB.js
@@ -3,10 +3,17 @@ import { connect } from 'react-redux';
 import { updateGroupThunk } from '../../store';
 
 function GroupFormB(props) {
+  const name = props.group.name;
+  console.log(name);
+
   const [state, setState] = React.useState({
     name: '',
     description: '',
   });
+
+  React.useEffect(() => {
+    setState(props.group);
+  }, [props.group]);
 
   function refreshPage() {
     window.location.reload(false);
@@ -14,44 +21,42 @@ function GroupFormB(props) {
   function handleSubmit(event) {
     event.preventDefault();
     props.updateGroup(props.group.id, props.user.id, state);
-    setState({
-      name: '',
-      description: '',
-    });
+
     refreshPage();
   }
 
-  console.log(props);
+  // console.log(state.name);
+  // console.log(props);
 
   function handleChange(event) {
     setState({ ...state, [event.target.name]: event.target.value });
   }
 
   return (
-    <div>
-      <div>Create new Group</div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Title</label>
-          <input
-            name="name"
-            type="text"
-            value={state.name}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div>
-          <label>Description</label>
-          <input
-            name="description"
-            type="text"
-            value={state.description}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <button type="submit">Edit</button>
-      </form>
-    </div>
+    <form className="groupviewC-content" onSubmit={handleSubmit}>
+      <div>
+        {/* <label>Title</label> */}
+        <input
+          className="groupviewC-name-input"
+          name="name"
+          type="text"
+          value={state.name}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <div>
+        <input
+          className="groupviewC-detail-input"
+          name="description"
+          type="text"
+          value={state.description}
+          onChange={handleChange}
+        ></input>
+
+        {/* <label>Description</label> */}
+      </div>
+      <button type="submit">Edit</button>
+    </form>
   );
 }
 
