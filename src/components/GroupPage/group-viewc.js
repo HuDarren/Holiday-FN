1096413316f1a13c5d537deb9554ddd5288a9502
Darren Hu <darren.hu3@gmail.com>
2020-12-15 13:22:21 -0500

    groupview components update state

--- src/components/GroupPage/group-viewc.js
+++ src/components/GroupPage/group-viewc.js
@@ -11,6 +11,10 @@ import GroupFollow from './group-follow';
 import GroupFormB from './group-formB';
 
 function GroupViewC(props) {
+  const [state, setState] = React.useState({
+    showForm: true,
+  });
+
   React.useEffect(() => {
     const target = props.history.location.pathname.split('/')[
       props.history.location.pathname.split('/').length - 1
@@ -18,22 +22,58 @@ function GroupViewC(props) {
     props.fetchGroup(target);
   }, []);
 
+  function edit() {
+    setState({
+      showForm: !state.showForm,
+    });
+  }
+
+  console.log('state', state.showForm);
+
   return (
     <div>
       <div>Groups</div>
-      <GroupFollow
-        unSubToGroup={props.unSubToGroup}
-        subToGroup={props.subToGroup}
-        group={props.group}
-        userId={props.user.id}
-      />
-      <div>{props.group.name}</div>
-      <div>{props.group.description}</div>
-      <div>form below</div>
-      <GroupFormB group={props.group} />
-      <div>Put the buttons here. </div>
-      <button onClick={() => props.removeGroup(props.group.id)}>Delete</button>
-      <button>Update</button>
+      {/* Group Information  */}
+      <div>
+        <div className="groupviewC-container1">
+          <div className="groupviewC-container2">
+            <img
+              className="groupviewC-image"
+              alt="img"
+              src={props.group.groupImg}
+            ></img>
+
+            {/* Follow component, Button to Join the group or unJoin or if you are the creator of the group  */}
+
+            <GroupFollow
+              unSubToGroup={props.unSubToGroup}
+              subToGroup={props.subToGroup}
+              group={props.group}
+              userId={props.user.id}
+            />
+
+            {/* Buttons to edit and delete  */}
+            <div>
+              <button onClick={edit}>Edit</button>
+              <button onClick={() => props.removeGroup(props.group.id)}>
+                Delete
+              </button>
+            </div>
+          </div>
+          {state.showForm ? (
+            <div className="groupviewC-content">
+              <div className="groupviewC-name">{props.group.name}</div>
+              <div className="groupviewC-detail">{props.group.description}</div>
+            </div>
+          ) : (
+            <GroupFormB group={props.group} />
+          )}
+        </div>
+      </div>
+
+      {/* Form component , to edit if you are the owner  */}
+
+      {/* User Joined Component, List of all the users who joined the group  */}
       <div>
         {props.group &&
         props.group.GroupFollow &&
