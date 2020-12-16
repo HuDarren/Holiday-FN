b0386433ede1bb6c796f336699e5fe1a414de779
Darren Hu <darren.hu3@gmail.com>
2020-12-15 22:35:31 -0500

    add user view component

---
+++ src/components/UserPage/user-view.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import UserProfileForm from './user-profileForm';
+
+function UserView(props) {
+  const [state, setState] = useState({
+    showForm: true,
+  });
+
+  function edit() {
+    setState({
+      showForm: !state.showForm,
+    });
+  }
+
+  return (
+    <div className="userview-container1">
+      <div>
+        <div className="userview-container2">
+          <img
+            className="userview-image"
+            alt="text"
+            src={props.user.profileImage}
+          ></img>
+        </div>
+        {state.showForm ? (
+          <div className="userview-content">
+            <div className="userview-name">{props.user.name}</div>
+            <div className="userview-email">{props.user.email}</div>
+            <button className="userview-button">
+              <span className="userview-edit" onClick={edit}>
+                Edit
+              </span>
+            </button>
+          </div>
+        ) : (
+          <UserProfileForm edit={edit} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+const mapState = (state) => ({
+  user: state.user,
+});
+
+export default connect(mapState)(UserView);
