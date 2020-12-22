5731652f365ffe2051167a77a6b9c4ae6ff89c62
Darren <darren.hu3@gmail.com>
2020-12-22 11:06:34 -0500

    update group form

--- src/components/GroupPage/group-formA.js
+++ src/components/GroupPage/group-formA.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { addGroupThunk } from '../../store';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function GroupFormA(props) {
   const [state, setState] = React.useState({
@@ -8,45 +10,102 @@ function GroupFormA(props) {
     description: '',
   });
 
+    const [startDate, setStartDate] = React.useState(new Date());
+    const [image, setImage] = React.useState('');
+
   function handleSubmit(event) {
     event.preventDefault();
-    props.addGroup(props.user.id, state);
+       let info = { ...state, exchangeDate: startDate , groupImg: image};
+    props.addGroup(props.user.id, info);
     setState({
       name: '',
       description: '',
     });
   }
 
-  console.log(props);
+
 
   function handleChange(event) {
     setState({ ...state, [event.target.name]: event.target.value });
   }
 
+
+    async function uploadImage(e) {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'uploadx');
+  
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/dsi0jbonx/image/upload',
+      {
+        method: 'Post',
+        body: data,
+      }
+    );
+
+    const file = await res.json();
+    setImage(file.secure_url);
+
+  }
+
   return (
     <div>
       <div>Create new Group</div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Title</label>
-          <input
-            name="name"
-            type="text"
-            value={state.name}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div>
-          <label>Description</label>
-          <input
-            name="description"
-            type="text"
-            value={state.description}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <button type="submit">Add</button>
-      </form>
+          <form className="groupviewC-content" onSubmit={handleSubmit}>
+      <div>
+        <label className="groupviewC-label">Import Photo</label>
+        <input
+          className="groupviewC-name-input"
+          name="image"
+          type="file"
+          onChange={uploadImage}
+        ></input>
+      </div>
+      <div>
+        <label className="groupviewC-label">Title</label>
+        <input
+          className="groupviewC-name-input"
+          name="name"
+          type="text"
+          value={state.name}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <div>
+        <label className="groupviewC-label">Budget</label>
+        <input
+          className="groupviewC-detail-input"
+          name="budget"
+          type="text"
+          value={state.budget}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <div>
+        <label className="groupviewC-label">Description</label>
+        <textarea
+          className="groupviewC-detail-textarea"
+          name="description"
+          type="text"
+          value={state.description}
+          onChange={handleChange}
+        ></textarea>
+      </div>
+      <label className="groupviewC-label">Time</label>
+      <DatePicker
+        className="groupviewC-detail-input"
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat="MM/dd/yyyy"
+      />
+      <div className="groupviewC-viewForm-button1">
+        <button className="groupviewC-viewForm-button2" type="submit">
+          Edit
+        </button>
+      </div>
+    </form>
     </div>
   );
 }
