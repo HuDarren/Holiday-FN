97561253a19c96726caf1387b5b0ec5f749ef6e8
Darren <darren.hu3@gmail.com>
2020-12-27 15:47:45 -0500

    add user photo and update css

--- src/components/UserPage/user-profile.js
+++ src/components/UserPage/user-profile.js
@@ -6,6 +6,13 @@ function UserProfile(props) {
   return (
     <div className="user-container">
       <div className="user-name">Welcome, {props.user.name}</div>
+      <div
+      className="user-image-container"
+      >
+        <img alt="img" src={props.user.profileImage}
+        className="user-image"
+        ></img>
+      </div>
       {/* <div className="user-name">{props.user.email}</div> */}
     </div>
   );
