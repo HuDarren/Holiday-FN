7cb0459866f8e3f7df2f30f19fd55a42e5a0f074
Darren <darren.hu3@gmail.com>
2020-12-29 16:54:22 -0500

    update landingPageView Component

---
+++ src/components/LandingPage/landingViewA.js
@@ -0,0 +1,64 @@
+import React from "react"
+import "./landingViewA.css";
+
+function landingViewA(){
+    return <div
+     className="landingA-container"
+     >
+        <div 
+        className="landingA-title">
+            <div
+            className="landingA-titleA"
+            > Here's to the Holidays! Discover something special! </div>
+            <div
+            className="landingA-titleB"
+            
+            >  Grant a wish today </div>
+           
+        </div>
+
+
+    <div class="container grid center">
+      <div>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingA-image"></img>
+        <div
+        className="landingA-text"
+        >Pet Supplies</div>
+
+      </div>
+      <div>
+         <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/bedding_kmymjv.jpg"} className="landingA-image"></img>
+        <div
+         className="landingA-text"
+        >Beddings</div>
+
+      </div>
+      <div>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/clothes_ctnj98.jpg"} className="landingA-image"></img>
+        <div
+         className="landingA-text"
+        >Clothes</div>
+      </div>
+            <div>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270912/games_wqvt3z.jpg"} className="landingA-image"></img>
+        <div
+         className="landingA-text"
+        >Games</div>
+      </div>
+            <div>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270912/books_volleq.jpg"} className="landingA-image"></img>
+        <div
+         className="landingA-text"
+        >Books</div>
+      </div>
+            <div>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/decor_qwdjxf.jpg"} className="landingA-image"></img>
+        <div
+         className="landingA-text"
+        >Decorations</div>
+      </div>
+    </div>
+    </div>
+}
+
+export default landingViewA
