67b54de010fa188af45e158ae4e28ba26a1826cf
Darren <darren.hu3@gmail.com>
2020-12-30 17:22:22 -0500

    update landingViewC Css

---
+++ src/components/LandingPage/landingViewC.js
@@ -0,0 +1,67 @@
+import React from "react"
+import "./landingViewC.css"
+
+function landingViewC() {
+    return (
+    <div
+    className="landingC-container">
+        <div
+        className="landingC-container2" >
+            <div
+            className="landingC-container3"
+            >
+                <div
+                className="landingC-container4"
+                >
+      <div
+      className="landingC-contentA"
+      >
+    <div
+    className="landingC-contentA1">
+    <div
+    className="landingC-contentA1-text1"
+    >Editors's Picks</div>
+    <div
+     className="landingC-contentA1-text2"
+    >Gift Ideas Under $30</div>
+    </div>
+      </div>
+           <div
+      className="landingC-content"
+      >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+      </div>
+           <div
+      className="landingC-content"
+      >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+      </div>
+           <div
+      className="landingC-content"
+      >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+      </div>
+           <div
+      className="landingC-content"
+      >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+      </div>
+           <div
+      className="landingC-content"
+      >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+      </div>
+           <div
+      className="landingC-content"
+      >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+      </div>   
+                </div>
+            </div>
+
+        </div>
+    </div>
+    )
+}
+
+export default landingViewC
