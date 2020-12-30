bd749ccc9b2079e23814885acb2b617bfdd5635b
Darren <darren.hu3@gmail.com>
2020-12-30 17:22:39 -0500

    update landingViewB css

---
+++ src/components/LandingPage/landingViewB.js
@@ -0,0 +1,75 @@
+import React from "react"
+import "./landingViewB.css"
+
+
+function landingViewB(){
+    return <div
+     className="landingB-container"
+     >
+        <div 
+        className="landingB-title">
+            <div
+            className="landingB-titleA"
+            > Popular Wishes </div>
+        </div>
+
+    <div class="containerB gridB centerB">
+      <div
+      className="landingB-content"
+      >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingB-image"></img>
+        <div
+        className="landingB-text"
+        >Pet Supplies</div>
+          <div
+        className="landingB-text"
+        >Description...</div>
+
+      </div>
+      <div
+        className="landingB-content"
+      >
+         <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/bedding_kmymjv.jpg"} className="landingB-image"></img>
+        <div
+         className="landingB-text"
+        >Beddings</div>
+
+      </div>
+      <div
+        className="landingB-content"
+      >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/clothes_ctnj98.jpg"} className="landingB-image"></img>
+        <div
+         className="landingB-text"
+        >Clothes</div>
+      </div>
+            <div
+              className="landingB-content"
+            >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270912/games_wqvt3z.jpg"} className="landingB-image"></img>
+        <div
+         className="landingB-text"
+        >Games</div>
+      </div>
+            <div
+              className="landingB-content"
+            >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270912/books_volleq.jpg"} className="landingB-image"></img>
+        <div
+         className="landingB-text"
+        >Books</div>
+      </div>
+            <div
+              className="landingB-content"
+            >
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/decor_qwdjxf.jpg"} className="landingB-image"></img>
+        <div
+         className="landingB-text"
+        >Decorations</div>
+      </div>
+    </div>
+    </div>
+}
+
+export default landingViewB
+
