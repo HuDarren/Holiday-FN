31d876649eb5e7199987fafaf925c44523d83319
Darren <darren.hu3@gmail.com>
2020-12-31 17:16:56 -0500

    update landingViewB

--- src/components/LandingPage/landingViewB.js
+++ src/components/LandingPage/landingViewB.js
@@ -10,62 +10,62 @@ function landingViewB(){
         className="landingB-title">
             <div
             className="landingB-titleA"
-            > Popular Wishes </div>
+            > Fabric and Crafts</div>
         </div>
 
     <div class="containerB gridB centerB">
       <div
       className="landingB-content"
       >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingB-image"></img>
-        <div
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448953/wall6_gpri1f.jpg"} className="landingB-image"></img>
+        {/* <div
         className="landingB-text"
-        >Pet Supplies</div>
-          <div
+        >Pet Supplies</div> */}
+          {/* <div
         className="landingB-text"
-        >Description...</div>
+        >Description...</div> */}
 
       </div>
       <div
         className="landingB-content"
       >
-         <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/bedding_kmymjv.jpg"} className="landingB-image"></img>
-        <div
+         <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448953/wall5_tjfl2f.jpg"} className="landingB-image"></img>
+        {/* <div
          className="landingB-text"
-        >Beddings</div>
+        >Beddings</div> */}
 
       </div>
       <div
         className="landingB-content"
       >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/clothes_ctnj98.jpg"} className="landingB-image"></img>
-        <div
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448952/wall4_a7tp8y.jpg"} className="landingB-image"></img>
+        {/* <div
          className="landingB-text"
-        >Clothes</div>
+        >Clothes</div> */}
       </div>
             <div
               className="landingB-content"
             >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270912/games_wqvt3z.jpg"} className="landingB-image"></img>
-        <div
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448952/wall1_w7gozk.jpg"} className="landingB-image"></img>
+        {/* <div
          className="landingB-text"
-        >Games</div>
+        >Games</div> */}
       </div>
             <div
               className="landingB-content"
             >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270912/books_volleq.jpg"} className="landingB-image"></img>
-        <div
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448952/wall3_dntgpv.jpg"} className="landingB-image"></img>
+        {/* <div
          className="landingB-text"
-        >Books</div>
+        >Books</div> */}
       </div>
             <div
               className="landingB-content"
             >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/decor_qwdjxf.jpg"} className="landingB-image"></img>
-        <div
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448952/wall2_nlih16.jpg"} className="landingB-image"></img>
+        {/* <div
          className="landingB-text"
-        >Decorations</div>
+        >Decorations</div> */}
       </div>
     </div>
     </div>
