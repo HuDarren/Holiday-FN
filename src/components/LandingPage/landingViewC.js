512be5f7a8d8b1c6c0e84da6dc80fa420771caeb
Darren <darren.hu3@gmail.com>
2020-12-31 17:17:11 -0500

    update images on landingViewC

--- src/components/LandingPage/landingViewC.js
+++ src/components/LandingPage/landingViewC.js
@@ -29,32 +29,32 @@ function landingViewC() {
            <div
       className="landingC-content"
       >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448797/candles_r04dau.jpg"} className="landingC-image"></img>
       </div>
            <div
       className="landingC-content"
       >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448797/cups_xai40q.jpg"} className="landingC-image"></img>
       </div>
            <div
       className="landingC-content"
       >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448797/watch_cqptag.jpg"} className="landingC-image"></img>
       </div>
            <div
       className="landingC-content"
       >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609449195/toy_uqmf1u.jpg"} className="landingC-image"></img>
       </div>
            <div
       className="landingC-content"
       >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448797/shoes_k22wsz.jpg"} className="landingC-image"></img>
       </div>
            <div
       className="landingC-content"
       >
-        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609270728/pet_supplies_gebu5n.png"} className="landingC-image"></img>
+        <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448797/ties_bnoa9l.jpg"} className="landingC-image"></img>
       </div>   
                 </div>
             </div>
