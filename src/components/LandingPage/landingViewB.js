d93437a1ffce5deb53bf2bd61fd05a8f72e5c7df
Darren <darren.hu3@gmail.com>
2021-01-02 15:47:25 -0500

    update landingViewB texts

--- src/components/LandingPage/landingViewB.js
+++ src/components/LandingPage/landingViewB.js
@@ -18,54 +18,38 @@ function landingViewB(){
       className="landingB-content"
       >
         <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448953/wall6_gpri1f.jpg"} className="landingB-image"></img>
-        {/* <div
-        className="landingB-text"
-        >Pet Supplies</div> */}
-          {/* <div
-        className="landingB-text"
-        >Description...</div> */}
 
       </div>
       <div
         className="landingB-content"
       >
          <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448953/wall5_tjfl2f.jpg"} className="landingB-image"></img>
-        {/* <div
-         className="landingB-text"
-        >Beddings</div> */}
+
 
       </div>
       <div
         className="landingB-content"
       >
         <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448952/wall4_a7tp8y.jpg"} className="landingB-image"></img>
-        {/* <div
-         className="landingB-text"
-        >Clothes</div> */}
+
       </div>
             <div
               className="landingB-content"
             >
         <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448952/wall1_w7gozk.jpg"} className="landingB-image"></img>
-        {/* <div
-         className="landingB-text"
-        >Games</div> */}
+   
       </div>
             <div
               className="landingB-content"
             >
         <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448952/wall3_dntgpv.jpg"} className="landingB-image"></img>
-        {/* <div
-         className="landingB-text"
-        >Books</div> */}
+ 
       </div>
             <div
               className="landingB-content"
             >
         <img alt="img" src={"https://res.cloudinary.com/dsi0jbonx/image/upload/v1609448952/wall2_nlih16.jpg"} className="landingB-image"></img>
-        {/* <div
-         className="landingB-text"
-        >Decorations</div> */}
+
       </div>
     </div>
     </div>
