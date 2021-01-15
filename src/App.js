247a6d2ed67d83d3c0ea7601dca1f088ded6cc61
Darren <darren.hu3@gmail.com>
2021-01-15 17:39:52 -0500

    update footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Routes from "./routes";
 import NavBar from "./components/NavBar/nav-home";
+import Footer from "./components/Footer/footer-home"
 
 function App() {
   return (
     <div>
       <NavBar />
       <Routes />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/footer-home.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./footer-z.css"
+
+function FooterHome() {
+    return <div
+    className="footer-container"
+    >
+        <div
+        className="footer-content"
+        >
+   <span
+           className="footer-text"
+           > &copy; 2021 Holiday </span>
+        </div>
+  
+    </div>
+}
+
+export default FooterHome
