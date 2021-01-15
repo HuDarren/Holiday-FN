3a19c557f379af3cd74ecf3d938cfb5875913571
Darren <darren.hu3@gmail.com>
2021-01-15 15:09:38 -0500

    add searchA component

--- src/components/GShopPage/gshop-searchA.js
+++ src/components/GShopPage/gshop-searchA.js
@@ -1,11 +1,38 @@
 import React from "react"
+import {connect} from "react-redux"
+import {addItemThunk} from "../../store"
 
 
 function GShopSearchA(props) {
     return <div>
-        <div>Items</div>
+        <img alt="img" src={props.image}></img>
+        <div>{props.name}</div>
+        <div>{props.price}</div>
+        <div>
+        <button
+        onClick={
+        function(){
+        let info = { name : props.name, Image: props.image }
+        props.addItem(props.wishListId,info)}
+                }
+                >Wish</button>
+                <button
+                  type="button"
+                     onClick={(e) => {
+                     e.preventDefault();
+                    window.location.href= props.official_url;
+                     }}
+         >Buy</button>
+        </div>
     </div>
 }
 
+const mapState = (state)=>({
+    user:state.user
+})
 
-export default GShopSearchA
+const mapDispatch =(dispatch)=>({
+    addItem: (wishListId,info)=> dispatch(addItemThunk(wishListId,info))
+})
+
+export default connect(mapState,mapDispatch)(GShopSearchA)
