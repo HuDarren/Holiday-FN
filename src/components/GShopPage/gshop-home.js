c72670219fadd26c4b7967416053b2995edb048b
Darren <darren.hu3@gmail.com>
2021-01-15 17:12:16 -0500

    moved shop component

--- src/components/GShopPage/gshop-home.js
+++ src/components/GShopPage/gshop-home.js
@@ -1,12 +1,21 @@
 import React from "react"
 import GShopSearch from "./gshop-search"
-import GShopSearchB from "./gshop-searchB"
+// import GShopSearchB from "./gshop-searchB"
+// import GShopSearchC from "./gshop-searchC"
 
-function GShopHome(){
+function GShopHome(props){
+
 
     return <div>
-       <GShopSearch/>
-       <GShopSearchB/>
+       <GShopSearch
+       wishListId={props.match.params.id}
+       />
+       {/* <GShopSearchB
+       wishListId={props.match.params.id}
+       />
+        <GShopSearchC
+       wishListId={props.match.params.id}
+       /> */}
     </div>
 }
 
