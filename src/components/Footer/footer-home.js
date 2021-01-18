39186bd1b5e37698a3aa498b5a10bb9e942b177d
Darren <darren.hu3@gmail.com>
2021-01-18 09:46:08 -0500

    update format

--- src/components/Footer/footer-home.js
+++ src/components/Footer/footer-home.js
@@ -1,19 +1,14 @@
-import React from "react"
-import "./footer-z.css"
+import React from 'react';
+import './footer-z.css';
 
 function FooterHome() {
-    return <div
-    className="footer-container"
-    >
-        <div
-        className="footer-content"
-        >
-   <span
-           className="footer-text"
-           > &copy; 2021 Holiday </span>
-        </div>
-  
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        <span className="footer-text"> &copy; 2021 Holiday</span>
+      </div>
     </div>
+  );
 }
 
-export default FooterHome
+export default FooterHome;
--- src/components/GShopPage/gshop-searchA.js
+++ src/components/GShopPage/gshop-searchA.js
@@ -1,90 +1,64 @@
-import React from "react"
-import {connect} from "react-redux"
-import {addItemThunk} from "../../store"
-import "./gshop-searchA.css"
-
+import React from 'react';
+import { connect } from 'react-redux';
+import { addItemThunk } from '../../store';
+import './gshop-searchA.css';
 
 function GShopSearchA(props) {
-
-    return (
-    <div
-     className="gshopA-container"
-    >
-    <div>
-    <div>
-    <div
-                
-                className="gshopA-content-container3"
-                >
-                <div
-                className="gshopA-content-container4"
-                >
-                <div
-                className="gshopA-image-container"
-                >
+  return (
+    <div className="gshopA-container">
+      <div>
+        <div>
+          <div className="gshopA-content-container3">
+            <div className="gshopA-content-container4">
+              <div className="gshopA-image-container">
                 <img alt="img" className="gshopA-image" src={props.image}></img>
+              </div>
+              <div className="gshopA-text-container">
+                <div className="gshopA-text">{props.name}</div>
+              </div>
+              <div className="gshopA-text-container">
+                <div className="gshopA-text">${props.price}</div>
+              </div>
+              <div className="gshopA-button-container">
+                <div className="gshopA-button-containerA">
+                  <button
+                    className="gshopA-button"
+                    onClick={function () {
+                      let info = { name: props.name, Image: props.image };
+                      props.addItem(props.wishListId, info);
+                    }}
+                  >
+                    <span className="gshopA-button-text">Wish</span>
+                  </button>
                 </div>
-                <div
-                className="gshopA-text-container"
-                >
-                <div
-                className="gshopA-text"
-                >{props.name}</div>
+                <div className="gshopA-button-containerA">
+                  <button
+                    className="gshopA-button"
+                    type="button"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      window.location.href = props.officialUrl;
+                    }}
+                  >
+                    {' '}
+                    <span className="gshopA-button-text">View</span>
+                  </button>
                 </div>
-                <div
-                  className="gshopA-text-container"
-                >
-                <div
-                className="gshopA-text"
-                >${props.price}</div>
-                </div>
-                <div 
-                className="gshopA-button-container"
-                >
-                    <div 
-                    className="gshopA-button-containerA">
-                <button
-                className="gshopA-button"
-                onClick={
-                    function(){
-                        let info = { name : props.name, Image: props.image }
-                        props.addItem(props.wishListId,info)
-                    }
-                }
-                >
-                    <span
-                    className="gshopA-button-text"
-                    >Wish</span>
-                    </button>
-                    </div>
-                             <div 
-                    className="gshopA-button-containerA">
-                <button
-                className="gshopA-button"
-                  type="button"
-                     onClick={(e) => {
-                     e.preventDefault();
-                    window.location.href= props.officialUrl;
-                     }}
-                >      <span
-                    className="gshopA-button-text"
-                    >View</span></button>
-                    </div>
-                </div>
-                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-    </div>
-    )
+  );
 }
 
-const mapState = (state)=>({
-    user:state.user
-})
+const mapState = (state) => ({
+  user: state.user,
+});
 
-const mapDispatch =(dispatch)=>({
-    addItem: (wishListId,info)=> dispatch(addItemThunk(wishListId,info))
-})
+const mapDispatch = (dispatch) => ({
+  addItem: (wishListId, info) => dispatch(addItemThunk(wishListId, info)),
+});
 
-export default connect(mapState,mapDispatch)(GShopSearchA)
+export default connect(mapState, mapDispatch)(GShopSearchA);
