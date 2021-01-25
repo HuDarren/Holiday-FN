4153ded378fdf4f0011a80c5837228e4674322c8
Darren <darren.hu3@gmail.com>
2021-01-25 13:34:23 -0500

    remove comments

--- src/components/GShopPage/gshop-home.js
+++ src/components/GShopPage/gshop-home.js
@@ -1,22 +1,12 @@
-import React from "react"
-import GShopSearch from "./gshop-search"
-// import GShopSearchB from "./gshop-searchB"
-// import GShopSearchC from "./gshop-searchC"
+import React from 'react';
+import GShopSearch from './gshop-search';
 
-function GShopHome(props){
-
-
-    return <div>
-       <GShopSearch
-       wishListId={props.match.params.id}
-       />
-       {/* <GShopSearchB
-       wishListId={props.match.params.id}
-       />
-        <GShopSearchC
-       wishListId={props.match.params.id}
-       /> */}
+function GShopHome(props) {
+  return (
+    <div>
+      <GShopSearch wishListId={props.match.params.id} />
     </div>
+  );
 }
 
-export default GShopHome
+export default GShopHome;
