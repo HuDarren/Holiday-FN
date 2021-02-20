b072a1eb9041622855437b1047366739dc0f20ff
Darren <darren.hu3@gmail.com>
2021-02-20 14:48:21 -0500

    css changes

--- src/components/GroupPage/group-viewC.js
+++ src/components/GroupPage/group-viewC.js
@@ -12,9 +12,7 @@ import GroupFormB from './group-formB';
 import SpinnerHome from '../SpinnerPage/spinner-home';
 import SpinnerView from '../SpinnerPage/spinner-view';
 
-
 function GroupViewC(props) {
-
   const [select, setSelect] = React.useState({
     participants: true,
     draw: false,
@@ -27,8 +25,6 @@ function GroupViewC(props) {
     ];
     props.fetchGroup(target);
   }, []);
-
-
 
   function viewP() {
     setSelect({
@@ -68,45 +64,48 @@ function GroupViewC(props) {
 
   return (
     <div>
-
       <div className="groupviewC-container3">
         <div className="groupviewC-container4">
-          <div className="groupviewC-name-container">
-            <div className="groupviewC-name">{props.group.name}</div>
-          </div>
-          <div className="groupviewC-image-container">
-            <img
-              className="groupviewC-image"
-              alt="img"
-              src={props.group.groupImg}
-            ></img>
-          </div>
-          <div className="groupviewC-content-container">
-            <div className="groupviewC-date-budget1">
-              <div className="groupviewC-date-budget2">Exchange On </div>
-              <div className="groupviewC-date-budget3">
-                {String(props.group.exchangeDate).slice(0,10)}
-              </div>
+          <div className="groupviewC-container5">
+            <div className="groupviewC-name-container">
+              <div className="groupviewC-name">{props.group.name}</div>
             </div>
+            <div className="groupviewC-image-container">
+              <img
+                className="groupviewC-image"
+                alt="img"
+                src={props.group.groupImg}
+              ></img>
+            </div>
+            <div className="groupviewC-content-container">
+              <div className="groupviewC-date-budget1">
+                <div className="groupviewC-date-budget2">Exchange On </div>
+                <div className="groupviewC-date-budget3">
+                  {String(props.group.exchangeDate).slice(0, 10)}
+                </div>
+              </div>
 
-            <div className="groupviewC-date-budget1">
-              <div className="groupviewC-date-budget2">Budget</div>
-              <div className="groupviewC-date-budget3">
-                ${props.group.budget}
+              <div className="groupviewC-date-budget1">
+                <div className="groupviewC-date-budget2">Budget</div>
+                <div className="groupviewC-date-budget3">
+                  ${props.group.budget}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="groupviewC-org-container">
-            <div className="groupviewC-org1">
-              <span className="groupviewC-org2">Organizer</span>
+            <div className="groupviewC-org-container">
+              <div className="groupviewC-org1">
+                <span className="groupviewC-org2">Organizer</span>
+              </div>
+              <div className="groupviewC-org1">
+                <div className="groupviewC-org3">{Owner()}</div>
+              </div>
+              <div></div>
             </div>
-            <div className="groupviewC-org1">
-              <div className="groupviewC-org3">{Owner()}</div>
+            <div className="groupviewC-detail-container">
+              <div className="groupviewC-detail2">
+                {props.group.description}
+              </div>
             </div>
-            <div></div>
-          </div>
-          <div className="groupviewC-detail-container">
-            <div className="groupviewC-detail2">{props.group.description}</div>
           </div>
         </div>
       </div>
